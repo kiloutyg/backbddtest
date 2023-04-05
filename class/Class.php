c8b0d21e0a8092b524ab08977bd743f39d22877f
kilo <carula@bk.ru>
2023-04-05 11:06:18 +0200

    change db name

--- class/Class.php
+++ class/Class.php
@@ -3,11 +3,17 @@
 
 class mysqli
 {
+    public  $servername;
+    public  $username;
+    public  $password;
+    public  $dbname;
+
     public function __construct($servername, $username, $password, $dbname)
     {
     }
 
     public function query($sql)
     {
+        $sql = "INSERT INTO users (name, first_name, date_of_birth, astrological_sign, password, complex_password) VALUES ('$name', '$firstName', '$dateOfBirth', '$astrologicalSign', '$password', '$complexPassword')";
     }
 }
