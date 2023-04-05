6c43a40664701470650f8871f311ff46833a10a5
kilo <carula@bk.ru>
2023-04-05 10:54:43 +0200

    tcho front

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
+<?php
+require 'user-creation.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <H1>Bonjour</H1>
+
+    <form action="get" class="form-control">
+        <input type="text" name="firstname" id="firstname">
+        <input type="text" name="lastname" id="lastname">
+    </form>
 </body>
+
 </html>
